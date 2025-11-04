744b1a9a2fd3be9f52f34690654aaaf799826321
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-04 13:01:09 +0530

    changes

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
 
-// function breakText () {
-// let heading = document.querySelector('.items h1');
-// let textContent = heading.textContent;
-
-// let textSplit = textContent.split('');
-
-// let halfValue = textSplit.length / 2;
-
-// let clutter = '';
-
-// textSplit.forEach((value, index) =>{
-//     if (index < halfValue) {
-//         clutter += `<span class = "a">${value}</span>`
-//     } else{
-//         clutter += `<span class = "b">${value}</span>`
-
-//     }
-//  heading.innerHTML = clutter;
-// });
-
-// }
-// breakText()
-
-// gsap.from('h1 .a', {
-//     opacity: 0,
-//     y: 80,
-//     duration : 0.6,
-//     delay: 0.5,
-//     stagger: 0.15
-// })
-// gsap.from('h1 .b', {
-//     opacity: 0,
-//     y: 80,
-//     duration : 0.6,
-//     delay: 0.5,
-//     stagger: -0.15
-// })
 
 gsap.from('.items h1' , {
     y: 100,
